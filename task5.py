c19413f32eb67dd42da4b163d40b3d7be324d36c
Aimen Rauf <aimen.rauf.2004@gmail.com>
2025-04-11 20:05:28 +0500

    task5.py

---
+++ task5.py
@@ -0,0 +1,35 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def add_child(self, child_node):
+        self.children.append(child_node)
+
+    def display(self, level=0):
+        print(" " * level * 4 + f"- {self.data}")
+        for child in self.children:
+            child.display(level + 1)
+
+    def levelNodes(self, target, current=0):
+        if current == target:
+            print(self.data)
+        for child in self.children:
+            child.levelNodes(target, current + 1)
+
+root = TreeNode("Root")
+child1 = TreeNode("Child1")
+child2 = TreeNode("Child2")
+child3 = TreeNode("Child3")
+
+root.add_child(child1)
+root.add_child(child2)
+root.add_child(child3)
+
+child1.add_child(TreeNode("GrandChild1"))
+child2.add_child(TreeNode("GrandChild2"))
+child3.add_child(TreeNode("GrandChild3"))
+
+root.display()
+print("Nodes at level 1:")
+root.levelNodes(1)
