756e3c0ae47142a86aef6326c00cfe9c4d62f780
Aimen Rauf <aimen.rauf.2004@gmail.com>
2025-04-11 20:11:23 +0500

    task6.py

---
+++ task6.py
@@ -0,0 +1,40 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def add_child(self, child_node):
+        self.children.append(child_node)
+
+    def display(self, level=0):
+        print(" " * level * 4 + f"- {self.data}")
+        for child in self.children:
+            child.display(level + 1)
+
+    def requirement(self):
+        result = []
+        if len(self.children) > 1:
+            result.append(self.data)
+        for child in self.children:
+            result += child.requirement()
+        return result
+
+root = TreeNode("Root")
+child1 = TreeNode("Child1")
+child2 = TreeNode("Child2")
+child3 = TreeNode("Child3")
+
+root.add_child(child1)
+root.add_child(child2)
+root.add_child(child3)
+
+child1.add_child(TreeNode("GrandChild1.1"))
+child1.add_child(TreeNode("GrandChild1.2"))
+child1.add_child(TreeNode("GrandChild1.3"))
+child2.add_child(TreeNode("GrandChild2.1"))
+child2.add_child(TreeNode("GrandChild2.2"))
+child3.add_child(TreeNode("GrandChild3"))
+
+root.display()
+nodes = root.requirement()
+print("Nodes with more than one child:", nodes)
